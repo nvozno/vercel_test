7051f7b965b6892b148c985c5fb44f7d9a8d09fe
null <null@null.com>
2022-06-24 14:14:57 +0800

    1

--- api/app.py
+++ api/app.py
@@ -1,11 +1,8 @@
-from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from flask import Flask, request
+import requests
 
-class handler(BaseHTTPRequestHandler):
+app = Flask(__name__)
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask Github!'
