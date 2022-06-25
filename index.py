08c063fc46c5cca49cafc646b730dcd81554a1d6
zhouhaosong <null@null.com>
2022-06-26 01:02:59 +0800

    1

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
-from flask import Flask
-
+from flask import Flask, Response
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return "<h2>Flask Vercel</h2>"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
